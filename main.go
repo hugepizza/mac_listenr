cf17ba294e9e2a640e2122f3c1d1ea142dc72c1c
wangleilei <wangleilei@idongde.com>
2019-12-17 19:46:44 +0800

    init

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	ms := getMacAddrs()
+	http.DefaultServeMux.HandleFunc("/mac", func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Set("Access-Control-Allow-Origin", "*")
+		resp.Write([]byte(strings.Join(ms, "\n")))
+	})
+	fmt.Println("运行中 访问植提桥后台时请保持运行 ...")
+	http.ListenAndServe(":9999", http.DefaultServeMux)
+}
+
+func getMacAddrs() (macAddrs []string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Printf("fail to get net interfaces: %v", err)
+		return macAddrs
+	}
+
+	for _, netInterface := range netInterfaces {
+		macAddr := netInterface.HardwareAddr.String()
+		if len(macAddr) == 0 {
+			continue
+		}
+
+		macAddrs = append(macAddrs, macAddr)
+	}
+	return macAddrs
+}
